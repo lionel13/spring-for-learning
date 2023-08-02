81ae35887fe13894efc6438e6f0fb9c1ee02249e
Lionel Audibert <laudibert@free.fr>
2023-08-02 17:30:07 +0200

    [SFL-009] add infrastructure module

---
+++ infrastructure/src/main/java/fr/varex13/student/StudentEntity.java
@@ -0,0 +1,48 @@
+package fr.varex13.student;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class StudentEntity {
+
+    @Id
+    private long id;
+    private UUID uuid;
+    private String firstName;
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ infrastructure/src/main/java/fr/varex13/student/StudentRepositoryPostgreSql.java
@@ -0,0 +1,17 @@
+package fr.varex13.student;
+
+import fr.varex13.student.outputport.StudentRepository;
+
+import java.util.Set;
+
+public class StudentRepositoryPostgreSql implements StudentRepository {
+    @Override
+    public void add(Student student) {
+
+    }
+
+    @Override
+    public Set<Student> all() {
+        return null;
+    }
+}
