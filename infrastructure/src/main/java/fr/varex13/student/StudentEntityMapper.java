1d31b7002b4269fc967039993040aa5b1cf93ab6
Lionel Audibert <laudibert@free.fr>
2023-09-18 12:36:07 +0200

    [SFL-009] implements StudentRepository + add testContainer

--- infrastructure/src/main/java/fr/varex13/student/StudentEntityMapper.java
+++ infrastructure/src/main/java/fr/varex13/student/StudentEntityMapper.java
@@ -1,32 +1,40 @@
 package fr.varex13.student;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class StudentEntityMapper {
+
+    private static Mapper<Student, StudentEntity> student = studentEntity -> Student.studentBuilder().id(studentEntity.getUuid()).firstName(studentEntity.getFirstName()).lastName(studentEntity.getLastName()).build();
+
+    private static Mapper<StudentEntity, Student> studentEntity = student -> {
+        final StudentEntity studentEntity = new StudentEntity();
+        studentEntity.setUuid(student.getId());
+        studentEntity.setFirstName(student.getFirstName());
+        studentEntity.setLastName(student.getLastName());
+        return studentEntity;
+    };
+
+    private static MapperList<Student, StudentEntity> students = studentEntities -> studentEntities.stream().map(studentEntity -> student.map(studentEntity)).toList();
+
+    private static MapperList<StudentEntity, Student> studentEntities = students -> students.stream().map(student -> studentEntity.map(student)).toList();
 
     private StudentEntityMapper() {
         throw new IllegalStateException("Utility class");
     }
 
     public static StudentEntity studentToStudentEntity(final Student student) {
-        final StudentEntity studentEntity = new StudentEntity();
-        studentEntity.setUuid(student.getId());
-        studentEntity.setFirstName(student.getFirstName());
-        studentEntity.setLastName(student.getLastName());
-        return studentEntity;
+        return studentEntity.map(student);
     }
 
-    public static Student studentEntityToStudent(final StudentEntity studentEntity) {
-        return Student.studentBuilder()
-                .id(studentEntity.getUuid())
-                .firstName(studentEntity.getFirstName())
-                .lastName(studentEntity.getLastName())
-                .build();
+    public static Student studentToStudentEntity(final StudentEntity studentEntity) {
+        return student.map(studentEntity);
     }
 
-    public static Set<Student> studentEntityToStudent(final List<StudentEntity> studentEntities) {
-        return studentEntities.stream().map(StudentEntityMapper::studentEntityToStudent).collect(Collectors.toSet());
+    public static List<StudentEntity> studentsToStudentEntities(final List<Student> student) {
+        return studentEntities.map(student);
+    }
+
+    public static List<Student> studentEntitiesToStudents(final List<StudentEntity> studentEntities) {
+        return students.map(studentEntities);
     }
 }
--- infrastructure/src/main/java/fr/varex13/student/StudentRepositoryPostgreSql.java
+++ infrastructure/src/main/java/fr/varex13/student/StudentRepositoryPostgreSql.java
@@ -2,9 +2,10 @@ package fr.varex13.student;
 
 import fr.varex13.student.outputport.StudentRepository;
 
+import java.util.HashSet;
 import java.util.Set;
 
-import static fr.varex13.student.StudentEntityMapper.studentEntityToStudent;
+import static fr.varex13.student.StudentEntityMapper.studentEntitiesToStudents;
 import static fr.varex13.student.StudentEntityMapper.studentToStudentEntity;
 
 public class StudentRepositoryPostgreSql implements StudentRepository {
@@ -22,6 +23,6 @@ public class StudentRepositoryPostgreSql implements StudentRepository {
 
     @Override
     public Set<Student> all() {
-        return studentEntityToStudent(zazaRepository.findAll());
+        return new HashSet<>(studentEntitiesToStudents(zazaRepository.findAll()));
     }
 }
